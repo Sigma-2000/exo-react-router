b556761fb49cee0f056648ac3b37150266a43d45
Amélie Guyot <amelieguyot7@gmail.com>
2024-04-08 10:47:10 +0200

    set-form-input

--- exo-react/src/pages/Page1.jsx
+++ exo-react/src/pages/Page1.jsx
@@ -1,9 +1,22 @@
-
+import {useState} from 'react';
 const Page1 = () => {
+    const [name, setName] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Nom saisi: ${name}`);
+      };
     return (
         <div>
-           Page 1
-        </div>
+           <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    </div>
     );
 };
 
